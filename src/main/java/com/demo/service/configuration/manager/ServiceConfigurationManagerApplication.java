cc345713c9f98bf378b22c996a5a00454bbbd526
dimitar.debochichki_egt <dimitar.debochichki@amusnet.com>
2025-03-21 13:54:33 +0200

    Refresh configurations using Spring Cloud Bus

--- src/main/java/com/demo/service/configuration/manager/ServiceConfigurationManagerApplication.java
+++ src/main/java/com/demo/service/configuration/manager/ServiceConfigurationManagerApplication.java
@@ -2,13 +2,9 @@ package com.demo.service.configuration.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableConfigServer
-@EnableDiscoveryClient
-@EnableFeignClients(basePackages = "com.demo")
 @SpringBootApplication
 public class ServiceConfigurationManagerApplication {
 
